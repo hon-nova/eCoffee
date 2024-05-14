5ea87dcf20499769ca5a3c4fbfe1d0237abfcd09
hon-nova <hon.tnguyen294@gmail.com>
2024-05-14 15:28:11 -0700

    added nav

---
+++ eCoffee/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+
+# Create your models here.
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+
+class User(AbstractUser):
+    pass    
---
+++ eCoffee/views.py
@@ -0,0 +1,68 @@
+from django.shortcuts import render
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
+from django.db import IntegrityError
+from django.http import HttpResponse, HttpResponseRedirect,JsonResponse
+from django.shortcuts import render,redirect,get_object_or_404
+from django.urls import reverse
+import logging
+from .models import User
+
+logging.basicConfig(level=logging.DEBUG)
+# Create your views here.
+
+def index(request):
+    logging.debug('index got invoked::')
+    return render(request,'eCoffee/index.html')
+
+def login_view(request):
+    if request.method == "POST":
+
+        # Attempt to sign user in
+        username = request.POST["username"]
+        password = request.POST["password"]
+        user = authenticate(request, username=username, password=password)
+
+        # Check if authentication successful
+        if user is not None:
+            login(request, user)
+            return HttpResponseRedirect(reverse("index"))
+        else:
+            return render(request, "eCoffee/login.html", {
+                "message": "Invalid username and/or password."
+            })
+    else:
+        return render(request, "eCoffee/login.html")
+
+
+def logout_view(request):
+    logout(request)
+    return HttpResponseRedirect(reverse("index"))
+
+
+def register(request):
+    if request.method == "POST":
+        username = request.POST["username"]
+        email = request.POST["email"]
+
+        # Ensure password matches confirmation
+        password = request.POST["password"]
+        confirmation = request.POST["confirmation"]
+        if password != confirmation:
+            return render(request, "eCoffee/register.html", {
+                "message": "Passwords must match."
+            })
+
+        # Attempt to create new user
+        try:
+            user = User.objects.create_user(username, email, password)
+            user.save()
+        except IntegrityError:
+            return render(request, "eCoffee/register.html", {
+                "message": "Username already taken."
+            })
+        login(request, user)
+        return HttpResponseRedirect(reverse("index"))
+    else:
+        return render(request, "eCoffee/register.html")
+    
