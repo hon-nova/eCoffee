29d79529e0b403427729cb3fa82c55c23dd624d6
hon-nova <hon.tnguyen294@gmail.com>
2024-07-11 16:12:52 -0700

    shipped: to Neon

--- finalCS50W/settings.py
+++ finalCS50W/settings.py
@@ -85,12 +85,37 @@ WSGI_APPLICATION = 'finalCS50W.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+# Set DEBUG to False for production
+DEBUG = False
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#         'NAME': 'ecoffeepg',
+#         'USER': 'honnguyen',
+#         'PASSWORD': '',
+#         'HOST': 'localhost',
+#         'PORT': '5432',        
+#         }
+# }
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': os.getenv('DB_NAME'),
+        'USER': os.getenv('DB_USER'),
+        'PASSWORD': os.getenv('DB_PASSWORD'),
+        'HOST': os.getenv('DB_HOST'),
+        'PORT': os.getenv('DB_PORT'),
     }
 }
+
+ALLOWED_HOSTS = ['your_neon_hosted_domain_or_IP']
 AUTH_USER_MODEL = "eCoffee.User"
 
 # Password validation
