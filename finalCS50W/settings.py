c1a6aecafc67ab6d7b49096f5e94782715ffdfa2
hon-nova <hon.tnguyen294@gmail.com>
2024-07-12 12:41:53 -0700

    fixed:DNS

--- finalCS50W/settings.py
+++ finalCS50W/settings.py
@@ -25,17 +25,15 @@ STRIPE_SECRET_KEY = os.getenv('STRIPE_SECRET_KEY')
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
-
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'django-insecure-=lfr$fgpcgiyrvwb8+e&v=7%tbf(pqtr^xt%f*1b2k%o6mqs=&'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
+
 ALLOWED_HOSTS = ['ecoffee.onrender.com']
-CSRF_TRUSTED_ORIGINS=['https://127.0.0.1:8000']
+# CSRF_TRUSTED_ORIGINS=['https://127.0.0.1:8000']
 
 
 # Application definition
